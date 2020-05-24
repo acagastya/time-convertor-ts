542094a4d796aa95e36ceec88229b77ba931d6d6
Agastya Chandrakant <acagastya@outlook.com>
2020-05-24 22:32:45 +0530

    Initial commit

---
+++ src/App/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <header className="page-header">
+        <h1>time-convertor</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
