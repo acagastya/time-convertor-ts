453836c9c943a42a64f20cb3c14d3ebc353d9c8b
Agastya Chandrakant <acagastya@outlook.com>
2020-05-25 06:33:44 +0530

    more util functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,16 +8,27 @@ function friendlyStr(str: string): string {
   return str.replace(/_/g, ' ');
 }
 
+/**
+ * @description Convert a string like `foo bar baz` to `foo_bar_baz`
+ * @param {string} str
+ */
+function unfriendlyStr(str: string): string {
+  return str.replace(/ /g, '_');
+}
+
 export {
-  friendlyStr
+  friendlyStr,
+  unfriendlyStr
 };
 
 const DEFAULT_TZ = 'UTC';
 const localTimezone = moment.tz.guess();
+const timezoneList = moment.tz.names().sort();
 const YEAR = new Date().getFullYear();
 
 export {
   DEFAULT_TZ,
   localTimezone,
+  timezoneList,
   YEAR
 };
