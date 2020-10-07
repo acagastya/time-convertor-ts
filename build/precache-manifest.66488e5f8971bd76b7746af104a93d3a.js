5edb80751c3dacdd13acaab6317fee68528026a6
Agastya Chandrakant <acagastya@outlook.com>
2020-10-07 18:08:49 +0530

    Add build folder

---
+++ build/precache-manifest.66488e5f8971bd76b7746af104a93d3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e77027d7f9ff2d613724f102647f77a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "402e4e3ffafe0a44f95a",
+    "url": "/static/js/2.87eda051.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.87eda051.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d1470162b1683eed5c1",
+    "url": "/static/js/main.09947b2f.chunk.js"
+  },
+  {
+    "revision": "860fc39487ef3edd155c",
+    "url": "/static/js/runtime-main.19ca923e.js"
+  }
+]);
